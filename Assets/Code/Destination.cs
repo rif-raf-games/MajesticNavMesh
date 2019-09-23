999f2932a2480016f8762a5b1eeeeaee0ec7d444
MikeRifRaf <m.oneil@rifrafgames.com>
2019-09-23 16:39:45 -0600

    Initial check in.  Open up the /Assets/Scenes/NavMestTest01 scene to test.

---
+++ Assets/Code/Destination.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+    public NPCMove Player;
+    public GameObject DestSphere;
+
+    private Vector3 CamOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Floor");
+       // foreach (GameObject o in objs) o.GetComponent<MeshRenderer>().enabled = false;
+        objs = GameObject.FindGameObjectsWithTag("Wall");
+      //  foreach (GameObject o in objs) o.GetComponent<MeshRenderer>().enabled = false;
+
+        CamOffset = Camera.main.transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            LayerMask mask = LayerMask.GetMask("Floor");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+            {                
+                Vector3 dest = hit.point;                                
+                DestSphere.transform.position = dest;
+                Player.SetDestination(dest);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Camera.main.transform.position = Player.transform.position + CamOffset;
+    }
+}
