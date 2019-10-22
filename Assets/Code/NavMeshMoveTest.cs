ddca89caef358971dff3443a6d577e2dab7be315
MikeRifRaf <m.oneil@rifrafgames.com>
2019-10-22 10:38:21 -0600

    Added support for testing nav mesh movement with any new ship geometry.

---
+++ Assets/Code/NavMeshMoveTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMoveTest : MonoBehaviour
+{
+    NavMeshAgent NavMeshAgent;
+    private Vector3 CamOffset;
+    public GameObject DestSphere;
+    public bool ShowDestSphere = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent = this.GetComponent<NavMeshAgent>();
+        CamOffset = Camera.main.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    public void SetDestination(Vector3 dest)
+    {
+        NavMeshAgent.SetDestination(dest);
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            LayerMask mask = LayerMask.GetMask("Floor");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+            {
+                Vector3 dest = hit.point;                
+                SetDestination(dest);
+                DestSphere.SetActive(ShowDestSphere);
+                if(ShowDestSphere == true )
+                {
+                    DestSphere.transform.position = dest;
+                }
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Camera.main.transform.position = this.transform.position + CamOffset;
+    }
+}
